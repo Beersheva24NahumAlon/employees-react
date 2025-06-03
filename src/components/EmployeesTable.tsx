a5cae919e9cad0002fb1c7e6e5e9332f64a65c4c
Nahum Alon <nochum.shepsl@gmail.com>
2025-06-03 22:16:26 +0300

    deleting employees without form and submit

--- src/components/EmployeesTable.tsx
+++ src/components/EmployeesTable.tsx
@@ -10,7 +10,6 @@ const EmployeesTable: React.FC = () => {
 
     const { data: employees, error, isLoading } = useEmployees();
     const mutation = useEmployeesMutation(id => apiClient.deleteEmployee(id as string))
-    const [id, setId] = useState("");
 
     return (
         isLoading ?
@@ -19,39 +18,37 @@ const EmployeesTable: React.FC = () => {
                 error?.message ?
                     <Text color="red">{error.message}</Text>
                     :
-                    <Box as="form" onSubmit={(event) => { event.preventDefault(); mutation.mutate(id); }}>
-                        <Table.Root size="sm">
-                            <Table.Header>
-                                <Table.Row>
-                                    <Table.ColumnHeader></Table.ColumnHeader>
-                                    <Table.ColumnHeader>Full Name</Table.ColumnHeader>
-                                    <Table.ColumnHeader>Birth Date</Table.ColumnHeader>
-                                    <Table.ColumnHeader>Department</Table.ColumnHeader>
-                                    <Table.ColumnHeader>Salary</Table.ColumnHeader>
-                                    <Table.ColumnHeader></Table.ColumnHeader>
+                    <Table.Root size="sm">
+                        <Table.Header>
+                            <Table.Row>
+                                <Table.ColumnHeader></Table.ColumnHeader>
+                                <Table.ColumnHeader>Full Name</Table.ColumnHeader>
+                                <Table.ColumnHeader>Birth Date</Table.ColumnHeader>
+                                <Table.ColumnHeader>Department</Table.ColumnHeader>
+                                <Table.ColumnHeader>Salary</Table.ColumnHeader>
+                                <Table.ColumnHeader></Table.ColumnHeader>
+                            </Table.Row>
+                        </Table.Header>
+                        <Table.Body maxHeight="75vh" overflow="auto">
+                            {employees?.map((empl) => (
+                                <Table.Row key={empl.id}>
+                                    <Table.Cell>
+                                        <Avatar.Root>
+                                            <Avatar.Fallback />
+                                            <Avatar.Image src={empl.avatar} alt={empl.fullName} />
+                                        </Avatar.Root>
+                                    </Table.Cell>
+                                    <Table.Cell>{empl.fullName}</Table.Cell>
+                                    <Table.Cell>{empl.birthDate}</Table.Cell>
+                                    <Table.Cell>{empl.department}</Table.Cell>
+                                    <Table.Cell>{empl.salary}</Table.Cell>
+                                    <Table.Cell>
+                                        <Button onClick={() => mutation.mutate(empl.id)}>Delete</Button>
+                                    </Table.Cell>
                                 </Table.Row>
-                            </Table.Header>
-                            <Table.Body maxHeight="75vh" overflow="auto">
-                                {employees?.map((empl) => (
-                                    <Table.Row key={empl.id}>
-                                        <Table.Cell>
-                                            <Avatar.Root>
-                                                <Avatar.Fallback />
-                                                <Avatar.Image src={empl.avatar} alt={empl.fullName} />
-                                            </Avatar.Root>
-                                        </Table.Cell>
-                                        <Table.Cell>{empl.fullName}</Table.Cell>
-                                        <Table.Cell>{empl.birthDate}</Table.Cell>
-                                        <Table.Cell>{empl.department}</Table.Cell>
-                                        <Table.Cell>{empl.salary}</Table.Cell>
-                                        <Table.Cell>
-                                            <Button type="submit" onClick={() => setId(empl.id || "")}>delete</Button>
-                                        </Table.Cell>
-                                    </Table.Row>
-                                ))}
-                            </Table.Body>
-                        </Table.Root>
-                    </Box>
+                            ))}
+                        </Table.Body>
+                    </Table.Root>
             }</>)
     )
 }
